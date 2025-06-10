67fafcd2eca878d2418da64297e4a554e4c2581c
Ankit Sinha <ankits.brainerhub@gmail.com>
2025-06-10 18:59:54 +0530

    fix the bug and added feature card section

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,40 +1,42 @@
-import React, { useRef } from "react";
-import { gsap } from "gsap/gsap-core";
+import { useRef } from "react";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const ShowcaseSection = () => {
-  const sectionRef = useRef();
-  const project1Ref = useRef();
-  const project2Ref = useRef();
-  const project3Ref = useRef();
+  const sectionRef = useRef(null);
+  const rydeRef = useRef(null);
+  const libraryRef = useRef(null);
+  const ycDirectoryRef = useRef(null);
 
   useGSAP(() => {
+    // Animation for the main section
     gsap.fromTo(
       sectionRef.current,
       { opacity: 0 },
       { opacity: 1, duration: 1.5 }
     );
-  }, []);
 
-  useGSAP(() => {
-    const projects = [
-      project1Ref.current,
-      project2Ref.current,
-      project3Ref.current,
-    ];
-    projects.forEach((item, index) => {
+    // Animations for each app showcase
+    const cards = [rydeRef.current, libraryRef.current, ycDirectoryRef.current];
+
+    cards.forEach((card, index) => {
       gsap.fromTo(
-        item,
-        { y: 50, opacity: 0 },
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
         {
           y: 0,
           opacity: 1,
-          duration: 1.5,
+          duration: 1,
           delay: 0.3 * (index + 1),
           scrollTrigger: {
-            trigger: item,
-            start: "top bottom = 100",
+            trigger: card,
+            start: "top bottom-=100",
           },
         }
       );
@@ -42,43 +44,46 @@ const ShowcaseSection = () => {
   }, []);
 
   return (
-    <section ref={sectionRef} id="work" className="app-showcase">
+    <div id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
-          {/* Left side */}
-          <div className="first-project-wrapper" ref={project1Ref}>
+          <div ref={rydeRef} className="first-project-wrapper">
             <div className="image-wrapper">
-              <img src="/images/project1.png" alt="project1" />
+              <img src="/images/project1.png" alt="Ryde App Interface" />
             </div>
             <div className="text-content">
               <h2>
-                On-demand Rides Simple With a Powerfull, User-friendly App
+                On-Demand Rides Made Simple with a Powerful, User-Friendly App
                 called Ryde
               </h2>
-              <p>
-                An app built with React/Vite & Tailwindcss for a fast user
-                friendly experience.
+              <p className="text-white-50 md:text-xl">
+                An app built with React Native, Expo, & TailwindCSS for a fast,
+                user-friendly experience.
               </p>
             </div>
           </div>
-          {/* Right side */}
-          <div className="project-list-wrapper">
-            <div className="project" ref={project2Ref}>
-              <div className="image-wrapper bg-[#ffefdb]">
-                <img src="/images/project2.png" alt="project2" />
+
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={libraryRef}>
+              <div className="image-wrapper bg-[#FFEFDB]">
+                <img
+                  src="/images/project2.png"
+                  alt="Library Management Platform"
+                />
               </div>
-              <h2>This is my project 2</h2>
+              <h2>The Library Management Platform</h2>
             </div>
-            <div className="project" ref={project3Ref}>
-              <div className="image-wrapper bg-[#ffe7eb]">
-                <img src="/images/project3.png" alt="project3" />
+
+            <div className="project" ref={ycDirectoryRef}>
+              <div className="image-wrapper bg-[#FFE7EB]">
+                <img src="/images/project3.png" alt="YC Directory App" />
               </div>
-              <h2>This is my project 3</h2>
+              <h2>YC Directory - A Startup Showcase App</h2>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
